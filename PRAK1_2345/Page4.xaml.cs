5fdc8187486b9ddf659d322beec260ad0737105e
Pavel Smirnov <pavelsmirnov954@gmail.com>
2023-11-22 09:21:51 +0300

    добавил задание со звездочкой

--- PRAK1_2345/Page4.xaml.cs
+++ PRAK1_2345/Page4.xaml.cs
@@ -24,5 +24,39 @@ namespace PRAK1_2345
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(NumberA.Text, out double a1))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            if (!double.TryParse(NumberB1.Text, out double b1))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            if (!double.TryParse(NumberC1.Text, out double c1))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            if (!double.TryParse(NumberA2.Text, out double a2))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            if (!double.TryParse(NumberB2.Text, out double b2))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            if (!double.TryParse(NumberC2.Text, out double c2))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            double d, x, y;
+            d = a1 * b2 - a2 * b1;
+            x = (c1 * b2 - c2 * b1) / d;
+            y = (a1 * c2 - a2 * c1) / d;
+
+            Answer123.Text += $"X = {x} \n Y = {y}";
+        }
     }
 }
